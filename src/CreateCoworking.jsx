61f684281392deddb6f9d603bd3c153a2496ed3b
David Robert <contact@david-robert.fr>
2023-07-11 13:50:13 +0200

    state form input

--- src/CreateCoworking.jsx
+++ src/CreateCoworking.jsx
@@ -1,35 +1,52 @@
+import { useState } from "react";
+
 const CreateCoworking = () => {
-  // fonction appelée par l'event listener
-  // elle prend automatiquement en paramètre
-  // un objet event qui contient des informations
-  // sur le submit du formulaire
+  // on créer trois states pour stocker les valeurs des inputs
+  // avec pour valeur par défaut une chaine de caractères vide
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [address, setAddress] = useState("");
+
   const handleSubmit = (event) => {
-    // empêcher le comportement par défaut du formulaire
-    // c'est à dire le rechargement de la page
     event.preventDefault();
 
-    // récupérer les valeurs des champs du formulaire
-    const title = event.target.title.value;
-    const description = event.target.description.value;
-    const address = event.target.address.value;
+    // on utilise les fonctions qui permettent de modifier
+    // les valeurs des states title, description et address
+    // en leur passant en paramètre la valeur des inputs
 
-    // afficher les données envoyées sous le formulaire
-    console.log(title + " " + description + " " + address);
+    // le composant est donc rechargée (re-rendu)
+    // et a accès aux nouvelles valeurs des states
+    setTitle(event.target.title.value);
+    setDescription(event.target.description.value);
+    setAddress(event.target.address.value);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>Titre</label>
-      <input type="text" name="title" />
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Titre</label>
+        <input type="text" name="title" />
 
-      <label>Description</label>
-      <textarea name="description" cols="30" rows="10"></textarea>
+        <label>Description</label>
+        <textarea name="description" cols="30" rows="10"></textarea>
 
-      <label>Adresse</label>
-      <input type="text" name="address" />
+        <label>Adresse</label>
+        <input type="text" name="address" />
 
-      <button type="submit">Créer</button>
-    </form>
+        <button type="submit">Créer</button>
+      </form>
+
+      {/* 
+		on affiche les valeurs des states : au premier chargement  
+		c'est vide. Quand on soumet le formulaire, les valeurs des
+		states sont mises à jour et le composant est rechargé
+		Les nouvelles valeurs sont donc affichées
+	*/}
+      <p>Vos infos : </p>
+      <p>Titre : {title}</p>
+      <p>Description : {description}</p>
+      <p>Adresse : {address}</p>
+    </div>
   );
 };
 
